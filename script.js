999a73456fd7e92a1057601a810bdbc2c88e1cd0
obtanium <164098315+obtaniumm@users.noreply.github.com>
2025-10-29 19:21:34 -0400

    Updated script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('a[href^="#"]');
-
-    links.forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-            const target = document.querySelector(this.getAttribute('href'));
-            if (target) {
-                target.scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'start'
-                });
-            }
-        });
-    });
-
-    console.log('OBTANIUM / INITIALIZED');
+    console.log('obtanium');
 });
